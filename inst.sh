6367e5b77d984b015713967f115e1636c98b8ebc
abicorios <yaroslav.moroz91@gmail.com>
2019-04-27 07:02:13 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,6 +2,6 @@
 pr=`pwd`
 cd
 #Thank AnLinux
-#pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/armhf/arch.sh && bash arch.sh
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/armhf/arch.sh && bash arch.sh
 cp $pr/postinst.sh ~/arch-fs/root/postinst.sh
 echo './postinst.sh'>>~/arch-fs/root/.profile
